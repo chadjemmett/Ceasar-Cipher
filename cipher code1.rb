6b3e92585bcda1f51512b90d02368ac03733c706
ceejaay <chad.jemmett@gmail.com>
2012-05-12 16:25:10 -0600

    This a basic working encoder.
    Still can't deal with spaces. Or anything that's not a letter.

--- cipher code1.rb
+++ cipher code1.rb
@@ -3,20 +3,22 @@
 #first we create the alphabet array.
 
 alphabet_array = ("a".."z").to_a
-#the rotate parameter is the shift number.
 
 #the other variables.
-message = "spzaluzayhunl"
+message = "mauraanddaddy"
 holding_array = []
 coded_message = String.new
 
 #the meat of the code. we get the numbers in the un-rotated array.
 message.each_char { |letter| holding_array << alphabet_array.index(letter)}
-puts holding_array
 
-puts "****************************"
+#the next two lines are just for testing and clarity. I was checking the holding array for the correct numbers.
+#puts holding_array
+
+#puts "****************************"
+
 #then we rotate the array.
-aplhabet_array = alphabet_array.rotate!(-7)
+aplhabet_array = alphabet_array.rotate!(20)
 
 #using the prior numbers we cram the letters into a new string.
 holding_array.each {|number| coded_message << alphabet_array[number]}
